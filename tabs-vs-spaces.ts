6466974e448ad0daabc02e30dc6a8a28a433ab6b
Grzegorz Cichosz <grzegorz.cichosz@noaignite.com>
2022-12-21 12:00:37 +0100

    feat: add tabs width answer

--- tabs-vs-spaces.ts
+++ tabs-vs-spaces.ts
@@ -1,4 +1,5 @@
 export const tabsVsSpaces = () => {
-  console.log('Tabs or spaces?');
-  console.log('If tab = x * space, then x === ?');
+    console.log("Tabs or spaces?");
+    console.log("If tab = x * space, then x === ?");
+    console.log("x === 4 of course!");
 };
